c2788474f32fd07fe5f6a490d67387ae6ff534d8
Kaya Tacer <ktacer@proton.me>
2025-04-29 16:09:36 -0400

    Added correctness evaluation

--- src/rag_eval/evaluation_loader.py
+++ src/rag_eval/evaluation_loader.py
@@ -1,8 +1,8 @@
 import json
 
 from deepeval import evaluate
-from deepeval.metrics import AnswerRelevancyMetric
-from deepeval.test_case import LLMTestCase
+from deepeval.metrics import AnswerRelevancyMetric, GEval
+from deepeval.test_case import LLMTestCase, LLMTestCaseParams
 
 from .rag_pipeline import LLM, RAGPipeline
 
@@ -56,6 +56,20 @@ class Evaluator():
 
         metrics = [
             AnswerRelevancyMetric(threshold=0.7, model="gpt-4o-mini"),
+            GEval(
+                name="Correctness",
+                evaluation_steps=[
+                    "If 'actual output' sufficiently answer the 'query' then it's a good output. 'Actual output' doesn't need to strictly repeat the information from 'expected output'"
+                    "Only use 'expected ouput' to check if the facts in 'actual output' contradicts any facts in 'expected output'",
+                    "If there is information in 'expected output' that isn't mentioned in the 'actual output', check it's relevant to the query to evaluate that information",
+                    "do not penalize the omission of insignificant words and relevant information",
+                    "actual output's having differnt writing style and grammar from the expected output's is OK"
+                ],
+                #criteria="Determine if the 'actual output' is factually correct based on the 'expected output'. Do not penalize for different format, structure, wording or unncessary information as long as the fact provided is correct"
+                model="gpt-4o-mini",
+                evaluation_params=[LLMTestCaseParams.ACTUAL_OUTPUT, LLMTestCaseParams.EXPECTED_OUTPUT],
+                threshold=0.5
+            )
         ]
 
         # Run evaluations
